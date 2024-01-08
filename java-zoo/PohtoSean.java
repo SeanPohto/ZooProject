16226f5a61258b3e3892a6dc602c64a51de6af51
SeanPohto <143440763+SeanPohto@users.noreply.github.com>
2024-01-08 11:36:40 -0800

    Add files via upload

---
+++ java-zoo/PohtoSean.java
@@ -0,0 +1,38 @@
+
+/**
+ * 
+ * 
+ * @author
+ * @version 
+ */
+public class PohtoSean extends Animal
+{
+  @Override
+  public String getStudentName() {
+      return "PohtoSean";
+  }
+  @Override 
+  public String getColor() {
+    return "orange";
+  }
+  @Override
+  public int getLegs() {
+      return 2;
+  }
+  @Override
+  public String speak() {
+      return "ooh-ooh-aah-aah";
+  }
+  @Override
+  public String getDisposition() {
+      return "arboreal";
+  }
+  @Override
+  public String getType() {
+    return "Orangutan";
+  }
+  @Override
+  public String getName() {
+      return "Greg";
+  }
+}
